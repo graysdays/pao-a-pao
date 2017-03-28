efb6a0e1c60b840c2e4edf54645d1318ed6a886e
Ciaran Edwards <ciaran.edwards@live.com>
2017-03-28 12:07:21 +0100

    Removed agenda references from restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :events, through: :agendas
-  has_many :agendas
+  has_many :events
   has_many :dishes, through: :menus
   has_many :menus
   has_many :users
