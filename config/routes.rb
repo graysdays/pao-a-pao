015da1c34a781e1958787d9fcb8bd02de6113b61
Pedro Rio <prio@teachme.site>
2017-03-28 12:48:23 +0100

    Added basic casein models without references

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+
+#Casein routes
+namespace :casein do
+    resources :restaurants
+    resources :dishes
+    resources :events
+end
+
   devise_for :users
   mount Attachinary::Engine => "/attachinary"
   get "home", to: "pages#home"
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
-    resources :restaurants
   end
 
   scope '(:locale)', locale: /en|ar/ do
